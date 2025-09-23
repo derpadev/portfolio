4786a741815d62ff1e2e0031b754cbcc3dd6ff2b
Devin Hua <devinhua004@gmail.com>
2025-09-22 19:22:23 -0700

    Initial portfolio setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import { ThemeToggle } from "../componenets/ThemeToggle";
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden ">
+
+        {/* Theme Toggle */}
+            <ThemeToggle />
+        {/* Background Effects*/}
+
+        {/* Nav Bar */}
+        
+        {/* Main Content */}
+
+        {/* Footer */}
+
+        </div>
+    );
+};
